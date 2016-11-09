c3493b0c56087f8be17b6404abcd4a0dc5304264
‘Matthew <matthew.lindly@hotmail.com>
2016-11-09 16:47:57 -0700

    Finished bonuses on 3.

--- expressServer.js
+++ expressServer.js
@@ -8,17 +8,12 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.use((req, res) => {
-  if(req.status === '404') {
-    res.sendStatus(404);
-  }
-});
-
 app.get('/pets', (req, res) => {
   fs.readFile(petsPath, 'utf8', (err, petsData) => {
     if (err) {
-      console.error(err.stack);
-      return res.sendStatus(404);
+      app.use((err, req, res, next) => {
+        next();
+      });
     }
 
     let pets = JSON.parse(petsData);
@@ -29,20 +24,37 @@ app.get('/pets', (req, res) => {
 app.get('/pets/:id', (req, res) => {
   fs.readFile(petsPath, 'utf8', (err, petsData) => {
     if (err) {
-      console.error(err.stack);
-      return res.sendStatus(404);
+      app.use((err, req, res, next) => {
+        next();
+      });
     }
 
     var id = Number.parseInt(req.params.id);
     let pets = JSON.parse(petsData);
 
     if (id < 0 || id >= pets.length || Number.isNaN(id)) {
-      return res.sendStatus(404);
+      app.use((err, req, res, next) => {
+        next();
+      });
     }
 
     res.set('Content-Type', 'text/plain');
     res.send(pets[id]);
   });
+});
+
+app.use((err, req, res, next) => {
+  if(err) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  }
+  else {
+    next();
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).send('Sorry cant find that!');
 });
 
 app.listen(port, () => console.log('Listening on port', port));
